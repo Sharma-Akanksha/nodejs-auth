15204d9aa817c2823950672ca45fb757f50f2c60
Akanksha Sharma <sharma.akanksha66@gmail.com>
2023-11-20 16:16:52 +0530

    updated server address

--- config/key.js
+++ config/key.js
@@ -6,7 +6,7 @@ module.exports = {
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/Nodejs-Auth');
+mongoose.connect('mongodb://0.0.0.0:27017/Nodejs-Auth');
 
 const db = mongoose.connection;
 
